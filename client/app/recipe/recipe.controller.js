c4dbce6f9a74adb20be3903c5e41b87bed39b2da
Stephane Bruckert <contact@stephanebruckert.com>
2015-08-31 13:59:14 +0200

    handle unexisting or removed recipe

--- client/app/recipe/recipe.controller.js
+++ client/app/recipe/recipe.controller.js
@@ -3,8 +3,8 @@
 angular.module('myBbcipesApp')
   .controller('RecipeCtrl', function($scope, $stateParams, $http) {
 
-    $http.get('/api/recipes/' + $stateParams.recipeId).success(function(awesomeRecipe) {
-      $scope.awesomeRecipe = awesomeRecipe;
-    });
+    $http.get('/api/recipes/' + $stateParams.recipeId).then(function(awesomeRecipe) {
+      $scope.awesomeRecipe = awesomeRecipe.data;
+    }, function(response) {});
 
   });
