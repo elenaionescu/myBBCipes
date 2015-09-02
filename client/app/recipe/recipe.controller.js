f6ae15d33c500cdd7266f23c42b76df679975977
Stephane Bruckert <contact@stephanebruckert.com>
2015-09-02 23:53:40 +0200

    add star/unstar ajax feature

--- client/app/recipe/recipe.controller.js
+++ client/app/recipe/recipe.controller.js
@@ -2,6 +2,10 @@
 
 var app = angular.module('myBbcipesApp');
 
+// ng-src equivalent for a background-image
+// also useful to avoid 404 error when browser 
+// tries to load the page before angular loaded.
+// http://stackoverflow.com/a/13782311/1515819
 app.directive('backImg', function() {
   return function(scope, element, attrs) {
     attrs.$observe('backImg', function(value) {
@@ -13,6 +17,8 @@ app.directive('backImg', function() {
   };
 });
 
+// Need a factory here because $http can't do PUT
+// and we need to star a recipe
 app.factory("Recipes", ['$resource',
   function($resource) {
     return $resource('/api/recipes/:recipeId', {
@@ -25,28 +31,49 @@ app.factory("Recipes", ['$resource',
   }
 ]);
 
-app.controller('RecipeCtrl', ['$scope', '$stateParams', '$http', 'User', '$cookieStore', '$resource', 'Recipes',
-  function($scope, $stateParams, $http, User, $cookieStore, $resource, Recipes) {
-    // get user information
-    var currentUser = {};
-    if ($cookieStore.get('token')) {
-      currentUser = User.get();
+app.controller('RecipeCtrl', ['$scope', '$stateParams', '$http', 'User', 'Auth', '$cookieStore', '$resource', 'Recipes',
+  function($scope, $stateParams, $http, User, Auth, $cookieStore, $resource, Recipes) {
+    // the recipe to display
+    $scope.awesomeRecipe = Recipes.get({
+      recipeId: $stateParams.recipeId
+    });
+
+    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+
+    // the action of tagging
+    $scope.starAction = function() {
+      if (!$scope.isLoggedIn()) {
+        alert("you are not connected");
+        return;
+      }
+      if (wasStarredByUser()) {
+        $scope.awesomeRecipe.stars--;
+      } else {
+        $scope.awesomeRecipe.stars++;
+      }
+
+      alert(JSON.stringify($scope.getCurrentUser(), null, 4));
+      Recipes.update({
+        recipeId: $stateParams.recipeId
+      }, $scope.awesomeRecipe);
     }
 
-    var recipe = Recipes.get({
-      recipeId: $stateParams.recipeId
-    });
-    recipe.stars = 33;
-    Recipes.update({
-      recipeId: $stateParams.recipeId
-    }, recipe);
+    $scope.getStarStatus = function() {
+      return $scope.getCurrentUser().recipes.indexOf($stateParams.recipeId) > -1 ? "star" : "unstar";
+    }
 
-    // get recipe information to display
-    $http.get('/api/recipes/' + $stateParams.recipeId).then(function(awesomeRecipe) {
-      $scope.awesomeRecipe = awesomeRecipe.data;
-    }, function(response) {
+    function wasStarredByUser() {
+      var index = $scope.getCurrentUser().recipes.indexOf($stateParams.recipeId);
+      if (index > -1) {
+        $scope.getCurrentUser().recipes.splice(index, 1);
+        return true;
+      } else {
+        $scope.getCurrentUser().recipes.unshift($stateParams.recipeId);
+        return false;
+      }
+    }
 
-    });
-    $scope.starred = "star";
+    //alert(JSON.stringify(YOUR_OBJECT_HERE, null, 4));
   }
 ]);
