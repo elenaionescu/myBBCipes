68320b265a0e1c86e9a529cf730ba30fb2ef7885
Stephane Bruckert <contact@stephanebruckert.com>
2015-09-04 16:46:07 +0200

    change color scheme

--- client/app/stars/stars.controller.js
+++ client/app/stars/stars.controller.js
@@ -4,14 +4,16 @@ angular.module('myBbcipesApp')
   .controller('StarsCtrl', function($scope, Auth, Recipes) {
     $scope.starredRecipes = [];
 
-    var starredRecipesId = Auth.getCurrentUser().recipes.toString().split(',');
-    $scope.allRecipes = Recipes.query(function() {
-      starredRecipesId.forEach(function(recipeId) {
-        $scope.allRecipes.forEach(function(recipe) {
-          if (recipe._id === recipeId) {
-            $scope.starredRecipes.unshift(recipe);
-          }
+    if (Auth.isLoggedIn()) {
+      var starredRecipesId = Auth.getCurrentUser().recipes.toString().split(',');
+      $scope.allRecipes = Recipes.query(function() {
+        starredRecipesId.forEach(function(recipeId) {
+          $scope.allRecipes.forEach(function(recipe) {
+            if (recipe._id === recipeId) {
+              $scope.starredRecipes.unshift(recipe);
+            }
+          });
         });
       });
-    });
+    }
   });
